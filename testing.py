fe0a5a4a3cae820cc901a449a698410f3ea9c538
dawoodahmedbutt <dawoodahmedbutt@gmail.com>
2025-01-06 16:03:06 +0000

    I have added a new file

---
+++ testing.py
@@ -0,0 +1,10 @@
+print('I am learning new things')
+
+
+# git init
+# git add . 
+
+# git config --global user.name "Your Name"
+# git config --global user.email "email"
+# git commit -m "Initial commit"
+# git 
